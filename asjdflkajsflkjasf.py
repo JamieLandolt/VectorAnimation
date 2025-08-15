126012b9cc505f73e4eb7623315f49a64dec65e6
Jasper Chong <74451969+jasperchongcode@users.noreply.github.com>
2025-08-15 22:22:22 +1000

    turn into class
    class MyHeart:
        def __init__(self, you):
            self.owner = you
            self.state = "fluttering"
            self.memories = []
    
        def feel(self, moment):
            self.memories.append(moment)
            return f"In your presence, my heart feels {moment}"
    
        def forever(self):
            while True:
                yield "love"

---
+++ asjdflkajsflkjasf.py
@@ -0,0 +1,93 @@
+from typing import List
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_average(vals: List[int]) -> int:
+    if len(vals) < 3:
+        return "error"
+    interval_width: int = 1
+    # trapezium method
+    middle = vals[1:-1]
+    # divide by b-a = len(int)-1
+    integral = 0.5*interval_width*(vals[0] + 2*sum(middle) + vals[-1])
+    return integral/(len(vals)-1)
+
+
+def get_c_j(x_y_vals, j: int) -> float:
+    # J is the c value we want to find
+
+    # first get the average of x and y independently
+
+    def fx(t): return x_y_vals[t][0]*np.cos(-2 *
+                                            np.pi*j*t) - x_y_vals[t][1]*np.sin(-2*np.pi*j*t)
+    def fy(t): return x_y_vals[t][1]*np.cos(-2 *
+                                            np.pi*j*t) + x_y_vals[t][0]*np.sin(-2*np.pi*j*t)
+
+    fx_vals = np.array([fx(t) for t in range(len(x_y_vals))])
+    fy_vals = np.array([fy(t) for t in range(len(x_y_vals))])
+
+    return (get_average(fx_vals), get_average(fy_vals))
+
+
+class VectorData:
+
+    def __init__(self, data: List[List[float]]):
+        """Data is of the form of a list of x and y tuples/lists"""
+        self.data = data
+        self.c_vals = self.get_c_vals()
+
+    def get_c_vals(self, min_j=-20, max_j=20):
+        self.c_vals = [(j, get_c_j(self.data, j))
+                       for j in range(min_j, max_j+1)]
+
+    def plot_data(self):
+        plt.scatter(x=self.data[:, 0], y=self.data[:, 1])
+        plt.axis('equal')
+        plt.show()
+
+    def sort_by_speed(self, asc=True):
+        """Assumes continuity between max and min j
+        sorts by speed ascending"""
+
+        # i = 1
+        output = []
+
+        zero_index = 0
+        # find the index of 0
+        while (self.c_vals[zero_index][0] != 0):
+            zero_index += 1
+
+        output.append(self.c_vals[zero_index])
+
+        i = 1
+
+        while zero_index-i >= 0 and zero_index+i < len(self.c_vals):
+            output.extend([self.c_vals[zero_index-i],
+                          self.c_vals[zero_index+i]])
+            i += 1
+
+        while zero_index-i >= 0:
+            output.append(self.c_vals[zero_index-i])
+            i += 1
+
+        while zero_index+i < len(self.c_vals):
+            output.append(self.c_vals[zero_index+i])
+            i += 1
+
+        if asc:
+            self.c_vals = output
+        else:
+            self.c_vals = output[::-1]
+
+    def sort_by_mag(c_data, byLargest):
+        def mag(tup): return tup[1][0]**2 + tup[1][1]**2
+        if (byLargest):
+            sortedMags = sorted(c_data, key=mag, reverse=True)
+        else:
+            sortedMags = sorted(c_data, key=mag)
+
+        return sortedMags
+
+    def get_c_vals(self):
+        return self.c_vals
