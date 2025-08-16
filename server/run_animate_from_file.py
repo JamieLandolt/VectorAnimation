d73d5c1e7da3ec42f208dee88cfd64f361ff834c
Jasper Chong <74451969+jasperchongcode@users.noreply.github.com>
2025-08-16 16:21:18 +1000

    add scaling to run animation

--- server/run_animate_from_file.py
+++ server/run_animate_from_file.py
@@ -4,9 +4,9 @@ import numpy as np
 import subprocess
 
 
-def run_animation(job_id: str = "data", manim_params='-p'):
+def run_animation(job_id: str = "data", manim_params='-pl'):
     data = np.load(f'points/{job_id}.npy')
-    data = np.array([[row[0]-450, -row[1]+350] for row in data])
+    data = np.array([[(row[0]-450)/20, (-row[1]+350)/20] for row in data])
     # Write to file read from
     np.save('data.npy', data)
     subprocess.Popen(
