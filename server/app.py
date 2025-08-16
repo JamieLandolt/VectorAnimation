578303b1b15f71fc48bf7f4cda606d8e645558d2
John Pioc <johnpioc15@gmail.com>
2025-08-16 13:12:14 +1000

    feat: added get and post endpoints for jobs in server

--- server/app.py
+++ server/app.py
@@ -1,10 +1,67 @@
 from flask import Flask, render_template
+from flask_restx import reqparse, Api, Resource
+import uuid
 
 app = Flask(__name__)
+api = Api(app, doc='/docs', prefix='/api')
 
+# WEBSITE ########################################################################################
 @app.route('/')
 def index():
     return render_template("index.html")
+
+# SERVER API #####################################################################################
+class Job():
+    def __init__(self, points):
+        self.id = str(uuid.uuid4())
+        self.points = points
+        self.is_done = False
+    
+    def get_id(self):
+        return self.id
+    
+    def get_points(self):
+        return self.points
+
+    def get_job_info(self):
+        return {
+            "job_id": self.id,
+            "is_done": self.is_done
+        }
+
+job_dict = {}
+
+job_info_args = reqparse.RequestParser()
+job_info_args.add_argument('job_id', type=str, location='json', required=True);
+
+job_creation_args = reqparse.RequestParser()
+job_creation_args.add_argument('points', type=list, location='json', required=True);
+
+@api.route('/job')
+class JobApi(Resource):
+    @api.expect(job_info_args)
+    def get(self):
+        args = job_info_args.parse_args()
+        job_id = args['job_id']
+
+        job = job_dict[job_id]
+        return job.get_job_info(), 200
+
+    @api.expect(job_creation_args)
+    def post(self):
+        args = job_creation_args.parse_args()
+        points = args['points']
+
+        new_job = Job(points);
+        new_job_id = new_job.get_id();
+        job_dict[new_job_id] = new_job
+
+        response = {
+            "job_id": new_job_id
+        }
+
+        return response, 201
+
 
 if __name__ == "__main__":
     app.run(debug=True)
